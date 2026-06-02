30adcd57053126d3d63d7b44c6e840221a8d356c
KokoroRay <namnguyen123456cm@gmail.com>
2026-06-02 07:42:59 +0700

    refactor: update PlatformSetting model to use UUID and remove deprecated fields

--- server/src/models/mysql/PlatformSetting.js
+++ server/src/models/mysql/PlatformSetting.js
@@ -3,27 +3,6 @@ const sequelize = require('../../config/database');
 
 const PlatformSetting = sequelize.define('PlatformSetting', {
   id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  platformFeePercent: {
-    type: DataTypes.DECIMAL(5, 2),
-    allowNull: false,
-    defaultValue: 5.00,
-  },
-  updatedBy: {
-    type: DataTypes.STRING,
-  },
-}, {
-  timestamps: true,
-});
-
-module.exports = PlatformSetting;const { DataTypes } = require('sequelize');
-const sequelize = require('../../config/database');
-
-const PlatformSetting = sequelize.define('PlatformSetting', {
-  id: {
     type: DataTypes.UUID,
     defaultValue: DataTypes.UUIDV4,
     primaryKey: true,
